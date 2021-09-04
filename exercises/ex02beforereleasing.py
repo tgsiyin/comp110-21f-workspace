25c2696454df210dce1aaa0be510771b07f25b83
Peiling He <tgsiyin@live.unc.edu>
2021-09-04 15:38:25 -0400

    ex02beforereleasingprogress

---
+++ exercises/ex02beforereleasing.py
@@ -0,0 +1,40 @@
+print("Your fortune cookie says...")
+furtune_random_number: int = randint(1, 30)
+SECRET_NUMBER_1: int = 8
+if furtune_random_number == SECRET_NUMBER_1:
+    print("A beautiful, smart, and loving person will be coming into your life.")
+else:
+    if furtune_random_number > SECRET_NUMBER_1:
+        print("Your life will be happy and peaceful.")
+    else:
+        if furtune_random_number < SECRET_NUMBER_1:
+            print("Soon life will become more interesting.")
+print("Now, go spread positive vibes!")
+
+count: int = 0
+repeat_words: str = input("What beat do you want to repeat?")
+print("What beat do you want to repeat?" + repeat_words)
+repeat_times: int = int(input("How many times you want to repeat?"))
+print("How many times you want to repeat?" + str(repeat_times))
+if repeat_times <= count:
+    print("No beat...")
+else: 
+    while count < repeat_times:
+        print(repeat_words)
+        count = count + 1
+
+input_letter: str = input("What letter do you want to seach for?: ")
+print("What letter do you want to seach for?: " + input_letter)
+input_word: str = input("Enter a word: ")
+print("Enter a word: " + input_word)
+length: int = len(input_word) - 1
+count_1: int = 0
+count_target: int = 0
+character: str = input_word[count_1]
+while length >= count_1:
+    if character == input_letter:
+        count_target = count_target + 1
+    else:
+        count_target = count_target
+    count_1 = count_1 + 1
+print("Count: " + str(count_target))
