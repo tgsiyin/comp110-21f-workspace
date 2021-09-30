7c02a05a758957b4819c20e7ea4facd26c051c99
Peiling He <tgsiyin@live.unc.edu>
2021-09-30 17:20:14 -0400

    ex05 progress

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -1,5 +1,44 @@
 """List utility functions part 2."""
 
-__author__ = "123456789"
+__author__ = "730272419"
 
 # Define your functions below
+
+
+def only_evens(x: list[int]) -> list[int]:
+    """The only_evens function to catch even numbers in a list."""
+    i: int = 0
+    a: list[int] = []
+    o: int
+    while i < len(x):
+        if x[i] % 2 == 0:
+            o = x[i]
+            a.append(o)
+        i += 1
+    return a
+
+
+def sub(x: list[int], a: int, b: int) -> list[int]:
+    """The sub function to catch specific places' list elements indicates by two ints."""
+    z: list[int] = []
+    o: int
+    p: int = len(x)
+    if len(x) <= 0 or b <= 0 or a >= p:
+        return []
+    if a < 0:
+        o = x[0]
+        z.append(o)
+    else:
+        f: int = x[a]
+        z.append(f)
+    if b >= p:
+        z.append(x[p - 1])
+    else:
+        z.append(b)
+    return z
+
+
+def concat(x: list[int], y: list[int]) -> list[int]:
+    """The concat function to concat two lists."""
+    z: list[int] = x + y
+    return z
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -3,4 +3,70 @@
 # TODO: Uncomment the below line when ready to write unit tests
 # from exercises.ex05.utils import only_evens, sub, concat
 
-__author__ = "123456789"
+__author__ = "730272419"
+from exercises.ex05.utils import only_evens
+from exercises.ex05.utils import sub
+from exercises.ex05.utils import concat
+
+
+def test_only_evens_evens_odds() -> None:
+    """Tests for the only_evens function."""
+    toe: list[int] = [2, 4, 1, 3]
+    assert only_evens(toe) == [2, 4]
+
+
+def test_only_evens_all_odds() -> None:
+    """Tests for the only_evens function."""
+    toe: list[int] = [1, 3]
+    assert only_evens(toe) == []
+
+
+def test_only_evens_all_evens() -> None:
+    """Tests for the only_evens function."""
+    toe: list[int] = [8, 8, 8]
+    assert only_evens(toe) == [8, 8, 8]
+
+
+def test_sub_use() -> None:
+    """Tests for sub function."""
+    tsub: list[int] = [10, 20, 30]
+    a: int = 0
+    b: int = 2
+    assert sub(tsub, a, b) == [10, 30]
+
+
+def test_sub_edge_1() -> None:
+    """Tests for sub function."""
+    tsub: list[int] = [10, 20, 30]
+    a: int = -2
+    b: int = 9
+    assert sub(tsub, a, b) == [10, 30]
+
+
+def test_sub_edge_2() -> None:
+    """Tests for sub function."""
+    tsub: list[int] = []
+    a: int = 9
+    b: int = 0
+    assert sub(tsub, a, b) == []
+
+
+def test_concat_edge() -> None:
+    """Tests for the concat function."""
+    tconcat1: list[int] = []
+    tconcat2: list[int] = []
+    assert concat(tconcat1, tconcat2) == []
+
+
+def test_concat_use1() -> None:
+    """Tests for the concat function."""
+    tconcat1: list[int] = [1, 2, 3]
+    tconcat2: list[int] = [4, 5, 6]
+    assert concat(tconcat1, tconcat2) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_use2() -> None:
+    """Tests for the concat function."""
+    tconcat1: list[int] = [1, 2, 3]
+    tconcat2: list[int] = []
+    assert concat(tconcat1, tconcat2) == [1, 2, 3]
